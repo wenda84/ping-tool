69ef9a7ecc7a6e9d1016a89393d2a492c255e63a
Jasson <zwd@local.com>
2023-03-03 10:55:01 +0800

    common modify

--- autoPush.cmd
+++ autoPush.cmd
@@ -6,12 +6,12 @@ title Github循环发布
 chcp 936 >nul 2>nul
 
 @REM =========================================  使用说明 begin  =========================================
-REM ADDR        ---- 表示需要ping的地址,
+REM DO_CMD      ---- 表示要执行的命令
 REM DO_FAIL     ---- 失败时要做的事情，比如设为一音乐文件地址，如需要则可以打开
 REM DELAY       ---- 命令执行的间隔
 REM LOG_ENABLE  ---- 是否产生本地日志
 set DO_CMD=git push
-set DO_FAIL=D:\TEMP\music\bg.mp3
+@REM set DO_FAIL=D:\TEMP\music\bg.mp3
 set DELAY=1
 set LOG_ENABLE=0
 @REM =========================================  使用说明 end =========================================
